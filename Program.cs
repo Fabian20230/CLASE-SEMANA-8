3bbac2d01bbc0afa35b6c3410d512c940089bd41
pc <pc@DESKTOP-2BEG0TC>
2023-04-01 08:54:16 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿void arrelos()
+{
+    string[] nombres = new string[]
+    {
+        "Fernanda","Melvin","Luis","Brandon","Yamilet"
+    };
+    //foreach (string nom in nombres)
+    //{
+    //    Console.WriteLine(nom);
+    }
+    int[] notas = new int[]{
+        70, 90, 43, 85, 30
+    };
+
+//int promedio = 0;
+//foreach (int i in notas)
+//{
+//    promedio = promedio + i;
+//}
+//promedio = promedio / notas.Length;
+//Console.WriteLine($"el promedio es:{promedio}");
+//}
+
+
+
+
+
+
+Console.WriteLine($"La nota mas alta fue de:{notas.Max()} con la puntuacion de:{notas.Max()} ");
+
+
+
+arrelos();
+
+
+
+
+
+
+
+
+
+
+
